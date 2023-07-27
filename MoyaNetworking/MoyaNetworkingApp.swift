d49df277e44f66f3bce9f223c5c70172800743ca
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-07-27 23:04:19 +0200

    Initial Commit

---
+++ MoyaNetworking/MoyaNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoyaNetworkingApp.swift
+//  MoyaNetworking
+//
+//  Created by Gagan Vishal  on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct MoyaNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
